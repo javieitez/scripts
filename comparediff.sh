47131a3b3859f1abfc324a9fadfd24c12728aa59
JA Vieitez <joseantonio.vieitez@gmail.com>
2019-04-02 13:02:11 +0200

    Create comparediff.sh

---
+++ comparediff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Author: JA Vieitez
+
+if [ $1 == "-h" ] || [ $1 == "--help" ]
+	then
+		echo "USAGE: comparediff fileA fileB outputfile"
+		echo "compares the content of fileA, line by line, with the content of fileB, and writes the output to outputfile"
+		exit 0
+fi
+
+for i in $(cat $1); do
+    grep $i $2
+	LASTEXITCODE=$? 
+	if [ $LASTEXITCODE == 0 ] 
+	then
+		# line existis in file B
+		echo "$i exists in $2"
+	else
+		echo "$i" >> $3
+fi
+done
