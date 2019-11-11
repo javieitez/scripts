de6636bbf44889ebba603b5ccf4e37920f9902e6
JA Vieitez <joseantonio.vieitez@gmail.com>
2019-11-11 11:00:48 +0100

    some refactoring for readability

--- check_url_pcre.sh
+++ check_url_pcre.sh
@@ -2,38 +2,25 @@
 
 # Author: JA Vieitez (github.com/javieitez)
 
-# Quick and dirty script to find a Perl Compatible Regex in the output of a URL. 
-# Unlike the bundled Regex functionality of check_http, this one supports multiline expressions :) 
-
+# Quick and dirty script to find a Perl Compatible Regex in the output of a URL. Unlike the bundled Regex functionality of check_http, this one supports multiline expressions :) 
 
 # help requested or invalid parameters
 #############################################################
-if  [ -z "$1" ]
+printhelp () {
+	echo "find a Perl compatible Regex in a web page"
+	echo ""
+	echo "Usage:  check_url_pcre hostname path REGEX [--http]"
+	echo ""
+	echo "HTTPS is enabled by default, use -HTTP to disable it"
+	echo "Example: check_url_pcre www.example.com /statistics.html '.*connected users.*\n.*([1-8][0-9]|90)'"
+	exit 3
+	}
+
+if [ "$1" == '-h' ] ||  [ "$1" == '--help' ]  || [ -z "$1" ] || [ -z "$2" ]
 	then 
-	echo "Invalid input. Please use --help for options."
-	exit 0
+	printhelp
 fi
 
-if [ "$1" == '-h' ] 
-	then 
-	NEEDHELP='true'
-fi
-
-if [ "$1" == '--help' ] 
-	then 
-	NEEDHELP='true'
-fi
-
-if [ "$NEEDHELP" == 'true' ] 
-	then
-		echo "find a Perl compatible Regex in a web page"
-		echo ""
-		echo "Usage:  check_url_pcre hostname path REGEX [--http]"
-		echo ""
-		echo "HTTPS is enabled by default, use -HTTP to disable it"
-		echo "Example: check_url_pcre www.example.com /statistics.html '.*connected users.*\n.*([1-8][0-9]|90)'"
-		exit 3
-fi
 #############################################################
 # Set to http if specified, default to https
 
@@ -44,11 +31,13 @@ if [ "$4" == "--http" ]
 fi
 
 #############################################################
+#Run the command
 curl --silent "$PROTOCOL://$1$2" | pcregrep -M "$3"
 
 #capture the exit code
 LASTEXITCODE=$?
 
+#return output based on the exit code
 if [ "$LASTEXITCODE" == 0 ] 
 	then
 		# No alarms? Ok, everything is right.
